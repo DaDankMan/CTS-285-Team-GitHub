d341aea2cd7e1533e76ba2be5ec4aaa5a75ce675
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-09-14 22:54:17 +0000

    9/14

---
+++ AnswerChecker.py
@@ -0,0 +1,48 @@
+def answerChecker():
+    # Set up game rules, loop starting condition, score, and total questions values
+    quit = False
+    print("This game will check to see if you have the correct answer to your equation. Enter q to quit and get your final score.")
+    score = 0
+    totalQuest = 0
+
+    # Start loop getting first equation    
+    while not quit:
+        num1 = int(input("Enter your first number: "))
+        operator = input("Operator: ")
+        num2 = int(input("Second number: "))
+        userAns = int(input("Your Answer: "))
+
+        # Check which operator is being used
+        if operator == "+":
+            ans = num1 + num2
+        elif operator == "-":
+            ans = num1 - num2
+        elif operator == "*":
+            ans = num1 * num2
+        elif operator == "/":
+            ans = num1 / num2
+        else:
+            print("Invalid operator.")
+            return
+        
+        # Check if user gave correct answer and adjust score if they did
+        if userAns == ans:
+            print("You got it right!")
+            score += 1
+        else:
+            print ("Your answer is incorrect.")
+            # Ask if user wants to see the correct answer
+            seeCorrect = input("Would you likme to see the correct answer? y/n: ")
+            if seeCorrect == "y":
+                print (ans)
+
+        # Increment total questions, print score and total questions, ask user to  continue or end loop
+        totalQuest += 1
+        print("Your score is ", score, "out of" , totalQuest)
+        x = input("Would you like to continue? Input 'q' to quit: ")
+        if x == "q":
+            quit = True
+            print ("Thanks for playing!")
+
+
+answerChecker()
