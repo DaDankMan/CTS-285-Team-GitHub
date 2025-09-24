a0e84d3f68fbe54108431eb1723b7f9c781b4b0a
DDM <110858016+DaDankMan@users.noreply.github.com>
2025-09-24 12:44:18 -0400

    Add files via upload
    Added a few comments to explain what's happening, fixed an if statement that prevents range from going under 10

--- numberGuesser.py
+++ numberGuesser.py
@@ -4,20 +4,21 @@ import random # Library that generates random numbers
 
 def main():
     
+    print("\nWelcome to the Number guesser!\n")
+    
     running = "yes"
-    wins = 0
-    totalGuesses = 0
+    wins = 0 # Counter that tracks how many times you've won
+    totalGuesses = 0 # Counter that tracks how many guesses you've made in total
     
     while running == "yes":
         
-        randNum = int(random.random() * 100) # Randomly generates a number from 0-100
+        randNum = int(random.random() * 100) # Randomly generates an integer from 0-100
         randRange = 50 # Range that will be added and subtracted from randNum
-        guesses = 0 # Self-explanatory
+        guesses = 0 # Counter that tracks how many guesses you've made this round
         
-        print("\nWelcome to the Number guesser!\n")
         print(f"The number is between {(randNum - randRange) - int(random.random()*5)} and {(randNum + randRange) + int(random.random()*5)}\n")
         
-        # Input validation
+        # Input validation, makes sure guess is an integer
         valid = False
         while valid == False:
             try:
@@ -27,18 +28,18 @@ def main():
             except ValueError:         
                 print("Invalid input. Please enter a valid integer")
         
-        while guess != randNum: # Loop for when the user is wrong
+        while guess != randNum: # Loop for when the user is wrong, kinda like input validation
             
             print("\nUh-oh, looks like you were wrong :(")
             guesses += 1
             totalGuesses += 1
             
-            if randRange >= 10: # Makes sure the range doesn't get too small
+            if randRange >= 20: # Makes sure the range doesn't go below 10
                 randRange -= 10
-                
+
             print(f"The number is between {(randNum - randRange) - int(random.random()*5)} and {(randNum + randRange) + int(random.random()*5)}\n")
-            
-            # Input validation
+
+            # Input validation, makes sure guess is an integer
             valid = False
             while valid == False:
                 try:
@@ -48,14 +49,15 @@ def main():
                 except ValueError:         
                     print("Invalid input. Please enter a valid integer") 
             
-            # If the user is correct
+            
+        # If the user is correct
         print("\nCorrect! You got the number!\n")
         print(f"Guesses taken: {guesses}")
         totalGuesses += 1
         wins += 1
         
         # If you wanna play again
-        running = input("Wanna play again? (Input yes/no): ")
+        running = input("Wanna play again? (Input yes/no): ").lower() #.lower() ensures that case doesn't matter
 
         
     # Ending stats
